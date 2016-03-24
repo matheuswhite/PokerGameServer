f05e2bd119baf8e2662063090c5f4960c097e361
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-03-24 12:12:58 -0300

    Add to string method

--- src/core/domain/game/Card.java
+++ src/core/domain/game/Card.java
@@ -11,6 +11,32 @@ public class Card {
 		this.number = number;
 	}
 	
-	
+	@Override
+	public String toString() {
+		String num = "";
+		
+		if (number > 1 && number <= 10) {
+			num = Integer.toString(number);
+		}
+		else {
+			switch (number) {
+			case 11:
+				num = "J";
+				break;
+			case 12:
+				num = "Q";
+				break;
+			case 13:
+				num = "K";
+				break;
+			case 14:
+				num = "A";
+				break;
+			default:
+				break;
+			}
+		}
+		return num + "of" + suit.toString();
+	}
 	
 }
--- src/core/domain/game/Suit.java
+++ src/core/domain/game/Suit.java
@@ -4,4 +4,26 @@ public enum Suit {
 
 	HEARTS , SPADES, DIAMONDS, CLUBS;   
 	
+	@Override
+	public String toString() {
+		String out = "";
+		switch (this) {
+		case HEARTS:
+			out = "Hearts";
+			break;
+		case SPADES:
+			out = "Spades";
+			break;
+		case DIAMONDS:
+			out = "Diamonds";
+			break;
+		case CLUBS:
+			out = "Clubs";
+			break;
+		default:
+			break;
+		}
+		
+		return out;
+	}
 }
