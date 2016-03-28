dfd1e6387a86914eebed06d9e7433326d3fb69b5
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-03-28 12:32:49 -0300

    Add a unit test to Algorithm class

---
+++ test/test/AlgorithmTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlgorithmTest {
+
+	@Test
+	public void testCheckHandRank() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountingCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasTwoPair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasOnePair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasThreeOfaKind() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasFourOfaKind() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasAs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsStraight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFlush() {
+		fail("Not yet implemented");
+	}
+
+}
