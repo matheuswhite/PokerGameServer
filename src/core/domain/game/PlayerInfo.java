319a44286748c36aeff39361caf6e864a8fc06ed
Matheus Tenorio <tenoriomatheus0@gmail.com>
2016-04-06 11:45:02 -0300

    add seat and id attribute

--- src/core/domain/game/PlayerInfo.java
+++ src/core/domain/game/PlayerInfo.java
@@ -4,16 +4,31 @@ import core.service.PrefixMultiplier;
 
 public class PlayerInfo {
 	
+	private long _id;
 	private Card[] _hand;
 	private Money _moneyPlayer;
+	private String _name;
+	private int _seat;
 	
-	public PlayerInfo() {
+	public PlayerInfo(long id) {
+		_id = id;
 		_hand = new Card[2];
 		_moneyPlayer = new Money(0, PrefixMultiplier.NONE);
+		_name = "Player" + id;
 	}
 	
 	public Card[] getHand() {
 		return _hand;
+	}
+	public void setHand(Card[] hand) {
+		if (hand.length != 2)
+			throw new IllegalArgumentException("Number of cards invalid");
+		
+		_hand = hand;
+	}
+	
+	public long getId() {
+		return _id;
 	}
 	
 	public Money getMoneyPlayer() {
@@ -24,5 +39,19 @@ public class PlayerInfo {
 	}
 	public void addMoney(Money money) {
 		_moneyPlayer.addMoney(money);
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	public void setName(String name) {
+		_name = name;
+	}
+	
+	public int getSeat() {
+		return _seat;
+	}
+	public void setSeat(int seat) {
+		_seat = seat;
 	}
 }
