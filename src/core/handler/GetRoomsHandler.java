907739fe86440451d08fd2b5164e5ce4b47ddd9b
DVDSON <dvdsoncomp@gmail.com>
2016-05-09 18:35:10 -0300

    Modificaco LeaveRooom e GetRoomsHandler

--- src/core/handler/GetRoomsHandler.java
+++ src/core/handler/GetRoomsHandler.java
@@ -2,13 +2,14 @@ package core.handler;
 
 import java.util.List;
 
+import core.domain.management.ServerManager;
 import core.handler.Handler;
 
 public class GetRoomsHandler extends Handler {
 
 	@Override
 	public void handle(List<Object> content) {
-		// TODO Auto-generated method stub
+		ServerManager server = (ServerManager) content.get(2);
 
 	}
 
--- src/core/handler/LeaveRoomHandler.java
+++ src/core/handler/LeaveRoomHandler.java
@@ -2,13 +2,20 @@ package core.handler;
 
 import java.util.List;
 
+import core.domain.management.ServerManager;
 import core.handler.Handler;
 
 public class LeaveRoomHandler extends Handler {
 
 	@Override
 	public void handle(List<Object> content) {
-		// TODO Auto-generated method stub
+		long roomID = (long) content.get(3);
+		long playerID = (long) content.get(1);
+		ServerManager server = (ServerManager) content.get(2);
+	
+		server.getRoom(roomID).removePlayer(server.getClient(playerID).getPlayerInfo());
+		
+
 
 	}
 
