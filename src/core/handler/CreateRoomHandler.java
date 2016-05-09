7d23b199afd476b953f3858a25e012fb134e30dc
DVDSON <dvdsoncomp@gmail.com>
2016-05-09 17:01:33 -0300

    Modificando Handler CreateRoom e ServerManager

--- src/core/handler/CreateRoomHandler.java
+++ src/core/handler/CreateRoomHandler.java
@@ -2,13 +2,17 @@ package core.handler;
 
 import java.util.List;
 
+import core.domain.game.Money;
+import core.domain.management.ServerManager;
 import core.handler.Handler;
 
 public class CreateRoomHandler extends Handler {
 
 	@Override
 	public void handle(List<Object> content) {
-		// TODO Auto-generated method stub
+		ServerManager server = (ServerManager) content.get(2);
+		
+		server.createRoom((Money) content.get(3),(Money) content.get(4));
 
 	}
 
