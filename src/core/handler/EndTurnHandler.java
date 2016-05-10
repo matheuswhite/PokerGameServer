a2166a6f1be540196d6d9fdb435b9faf6fd876a7
DVDSON <dvdsoncomp@gmail.com>
2016-05-10 17:44:30 -0300

    Atualizando UpdateMoneyHandler, EndTurnHandler e ServerManager de acordo

--- src/core/handler/EndTurnHandler.java
+++ src/core/handler/EndTurnHandler.java
@@ -14,12 +14,9 @@ public class EndTurnHandler extends Handler {
 		
 		//Esta classe so trata a passagem de turno do jogador
 		//Essas ações são feitas pela classe UpdateMoneyHandler
-		String action = (String) content.get(3);
-		Money moneyAdd = (Money) content.get(4);
 		long playerID = (long) content.get(1);
 		ServerManager server = (ServerManager) content.get(2);
 	
-		System.out.println("O Jogador id:" + playerID + "decidiu agir com " + action);
 		
 		Room room = server.getRoom(server.getClient(playerID).getCurrentRoomId());
 		room.getMatchInfo().increasePotValue(moneyAdd);
--- src/core/handler/UpdateMoneyHandler.java
+++ src/core/handler/UpdateMoneyHandler.java
@@ -1,17 +1,48 @@
 package core.handler;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import core.domain.game.Money;
+import core.domain.management.Room;
+import core.domain.management.ServerManager;
 import core.handler.Handler;
+import core.net.Message;
 
 public class UpdateMoneyHandler extends Handler {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void handle(List<Object> content) {
-		// TODO Auto-generated method stub
 		
-		//Esta classe vai tratar as ações do jogador
-		//CALL	RAISE	FOLD	BUY_IN
+		long playerID = (long) content.get(1);
+		ServerManager server = (ServerManager) content.get(2);
+		String action = (String) content.get(3);	
+		
+		Room room = server.getRoom(server.getClient(playerID).getCurrentRoomId());
+		
+		@SuppressWarnings("rawtypes")
+		List newContent = new ArrayList<Object>();
+		newContent.add(server.getClient(playerID).getPlayerInfo());
+		
+		if(action == "FOLD"){
+			//Espaco para invocar a funcao playerFold() em matchinfo ou Room
+			error
+			
+		}else{
+			Money moneyAdd = (Money) content.get(4);
+			room.getMatchInfo().increasePotValue(moneyAdd);
+		}
+		
+		newContent.add(room.getMatchInfo());
+		Message msg = new Message(action, newContent);
+		try {
+			server.sendPlayersInRoom(room.getId(),msg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		//Enviar a todos os jogadores da sala a ação deste jogador
 		//e o proximo estado da sala
