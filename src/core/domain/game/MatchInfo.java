547f987d41f62e6f06b61b3344d382744a1d65e4
DVDSON <dvdsoncomp@gmail.com>
2016-05-11 02:48:53 -0300

    Criado função nextPhase

--- src/core/domain/game/MatchInfo.java
+++ src/core/domain/game/MatchInfo.java
@@ -1,29 +1,31 @@
 package core.domain.game;
 
-import core.service.PrefixMultiplier;
-
 public class MatchInfo{
 	
 	private MatchPhase _currentMatchPhase;
-	private PlayerInfo _currentTurnPlayer;
-	private PlayerInfo _smallBlindPlayer;
-	private PlayerInfo _bigBlindPlayer;
+	private Long _currentTurnPlayerId;
+	private Long _smallBlindPlayerId;
+	private Long _bigBlindPlayerId;
+	private Long _dealerPlayerId;
 	private Card[] _cardsInTable;
 	private int _numCardsInTable;
 	private Money _smallBlindValue;
 	private Money _minimumBuyIn;
 	private Money _potValue;
+	private Money _higherCurrentBet;
 	
 	public MatchInfo() {
 		_currentMatchPhase = MatchPhase.PRE_FLOP;
-		_currentTurnPlayer = null;
-		_smallBlindPlayer = null;
-		_bigBlindPlayer = null;
+		_currentTurnPlayerId = null;
+		_smallBlindPlayerId = null;
+		_bigBlindPlayerId = null;
+		_dealerPlayerId = null;
 		_cardsInTable = new Card[5];
 		_numCardsInTable = 0;
 		_smallBlindValue = new Money();
 		_minimumBuyIn = new Money();
 		_potValue = new Money();
+		_higherCurrentBet = new Money();
 	}
 	
 	public MatchInfo(Money smallBlindValue, Money minimumBuyIn) {
@@ -32,6 +34,17 @@ public class MatchInfo{
 		_minimumBuyIn = minimumBuyIn;
 	}
 	
+	public MatchPhase nextPhase(MatchPhase actualPhase){
+		if(actualPhase == MatchPhase.PRE_FLOP)
+			return MatchPhase.FLOP;
+		if(actualPhase == MatchPhase.FLOP)
+			return MatchPhase.TURN;
+		if(actualPhase == MatchPhase.TURN)
+			return MatchPhase.RIVER;
+		return null;
+	}
+	
+	
 	public MatchPhase getCurrentMatchPhase() {
 		return _currentMatchPhase;
 	}
@@ -39,25 +52,32 @@ public class MatchInfo{
 		_currentMatchPhase = currentMatchPhase;
 	}
 	
-	public PlayerInfo getCurrentTurnPlayer() {
-		return _currentTurnPlayer;
+	public long getCurrentTurnPlayerId() {
+		return _currentTurnPlayerId;
 	}
-	public void setCurrentTurnPlayer(PlayerInfo currentTurnPlayer) {
-		_currentTurnPlayer = currentTurnPlayer;
-	}
-	
-	public PlayerInfo getSmallBlindPlayer() {
-		return _smallBlindPlayer;
-	}
-	public void setSmallBlindPlayer(PlayerInfo smallBlindPlayer) {
-		_smallBlindPlayer = smallBlindPlayer;
+	public void setCurrentTurnPlayerId(long currentTurnPlayerId) {
+		_currentTurnPlayerId = currentTurnPlayerId;
 	}
 	
-	public PlayerInfo getBigBlindPlayer() {
-		return _bigBlindPlayer;
+	public long getSmallBlindPlayerId() {
+		return _smallBlindPlayerId;
 	}
-	public void setBigBlindPlayer(PlayerInfo bigBlindPlayer) {
-		_bigBlindPlayer = bigBlindPlayer;
+	public void setSmallBlindPlayerId(long smallBlindPlayerId) {
+		_smallBlindPlayerId = smallBlindPlayerId;
+	}
+	
+	public long getDealerPlayerId() {
+		return _dealerPlayerId;
+	}
+	public void setDealerPlayerId(long dealerPlayerId) {
+		_dealerPlayerId = dealerPlayerId;
+	}
+	
+	public long getBigBlindPlayerId() {
+		return _bigBlindPlayerId;
+	}
+	public void setBigBlindPlayerId(long bigBlindPlayerId) {
+		_bigBlindPlayerId = bigBlindPlayerId;
 	}
 	
 	public Money getSmallBlindValue() {
@@ -73,9 +93,11 @@ public class MatchInfo{
 	}
 	public void increasePotValue(Money value) {
 		_potValue.addMoney(value);
+		if(value.getValue() > _higherCurrentBet.getValue())
+			_higherCurrentBet = value;
 	}
 	public void clearPot() {
-		_potValue = new Money(0, PrefixMultiplier.NONE);
+		_potValue = new Money();
 	}
 	
 	public Card[] getCardsInTable() {
@@ -91,5 +113,12 @@ public class MatchInfo{
 		for (int i = 0; i < 5; i++) {
 			_cardsInTable[i] = null;
 		}
+	}
+	
+	public Money getHigherCurrentBet() {
+		return _higherCurrentBet;
+	}
+	public void setHigherCurrentBet(Money higherCurrentBet) {
+		_higherCurrentBet = higherCurrentBet;
 	}
 }
